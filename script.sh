2fec69664a0e942d7a7047e53c90b05d0daef174
hamo050 <dzh7438@yandex.ru>
2023-07-14 19:27:53 +0300

    added rmdir comment

--- script.sh
+++ script.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# create dirs from N to N+10
+# to delete use rmdir {1..10}
+
+cur=1
+while [[ "$cur" -le 10 ]]
+do
+	if [[ -e "$cur" ]]
+	then
+		echo File $cur already exists
+		cur=$((cur + 1 ))
+		continue
+	fi
+	mkdir "$cur"
+	cur=$(( cur + 1 ))
+done
